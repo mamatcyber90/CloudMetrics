cefc4994db336ef19edaf44de25c522f7cef0050
tatchley <thomas.atchley@unitasglobal.com>
2017-12-07 11:14:57 -0600

    updated parser to use json format and send latency

--- benchmarks/fio/data/parser.py
+++ benchmarks/fio/data/parser.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-author: thomas.atchley
-"""
-
 import pandas
 import sys
 import os
@@ -18,7 +12,6 @@ import requests
 import base64
 import hmac
 
-# Look for parameters
 test_type = sys.argv[1]
 cloud = sys.argv[2]
 instance_type = sys.argv[3]
@@ -26,44 +19,42 @@ region = sys.argv[4]
 secret_key = sys.argv[5]
 ingress_url = sys.argv[6]
 os = sys.argv[7]
+interval = sys.argv[8]
+jobfile = sys.argv[9]
 
-raw_results = open("results.txt", "r")
 
-start_strings = list()
-end_strings = list()
+start_times = list()
+end_times = list()
 iops = list()
+latency = list()
 
-# Split results into seperate files
-for line in raw_results:
-    if (line.find('(groupid=') != -1):
-        print("hi")
-        start_strings.append(" ".join(line.split()[7:])) # gets start time
-    elif (line.find('mixed') == 2):
-        iops.append(line.split()[3][5:]) # add iops value
-        end_strings.append(int(line.split()[-1][:-4])) # add the miliseconds to end strings for now
+with open("fio.json") as fulltext:
+    tests=fulltext.read().replace("}\n{","}\n*\n{").split("*")
+
+    for i in range(len(tests)):
+        fiojson = json.loads(tests[i])
+
+        start_times.append(datetime.datetime.fromtimestamp(int(fiojson['timestamp'])))
+        end_times.append(start_times[i]+datetime.timedelta(seconds=int(interval)))
+        iops.append(fiojson['jobs'][0]['mixed']['iops'])
+        latency.append(fiojson['jobs'][0]['mixed']['lat']['mean'])
+
+start_iso = [date.isoformat() for date in start_times]
+end_iso = [date.isoformat() for date in end_times]
+
+iops_samples = list(zip(start_iso,end_iso,iops))
+latency_samples = list(zip(start_iso,end_iso,latency))
 
 
-raw_results.close()
+def sendResult(filename,samples, starttime, test_type):
 
-# Read results into Python
+    global ingress_url
+    global instance_type
+    global cloud
+    global region
+    global os
 
-# get python date objects from start/end times
-start_objs = [datetime.datetime.strptime(date, "%b %d %H:%M:%S %Y") for date in start_strings]
-end_objs = list()
-for i in range(len(start_objs)):
-        end_objs.append(start_objs[i]+datetime.timedelta(milliseconds=end_strings[i]))
-
-# Convert Python date objects into ISO 8601 format
-
-start_iso_list = [obj.isoformat() for obj in start_objs]
-end_iso_list = [obj.isoformat()[:-7] for obj in end_objs] # removes miliseconds
-
-### Build JSON Body ###
-
-# convert string into a python object
-samples = list(zip(start_iso_list,end_iso_list,iops))
-
-body = {"batch_started":start_iso_list[0],
+    body = {"batch_started":starttime,
         "test_type": test_type,
         "instance_type": instance_type,
         "cloud_provider": cloud,
@@ -71,21 +62,29 @@ body = {"batch_started":start_iso_list[0],
         "os": os,
         "samples":samples}
 
-prettybody = json.dumps(body, indent=4)
-signature = base64.b64encode(hmac.new(secret_key.encode(), prettybody.encode(), digestmod=hashlib.sha256).digest()).decode()
+    prettybody = json.dumps(body, indent=4)
+    signature = base64.b64encode(hmac.new(secret_key.encode(), prettybody.encode(), digestmod=hashlib.sha256).digest()).decode()
 
-print("The Signature is: {}".format(signature))
 
-# Post to recieving server
+    print("The Signature is: {}".format(signature))
 
-headers = {"X-UG-Signature":signature,
+    # Post to recieving server
+
+    headers = {"X-UG-Signature":signature,
            "Accept":"application/json",
            "Content-Type":"application/json"}
 
-responce = requests.post(ingress_url,data=prettybody, headers=headers)
-print("Responce code: {}".format(responce.status_code))
+    responce = requests.post(ingress_url,data=prettybody, headers=headers)
+    print("Responce code: {}".format(responce.status_code))
 
-with open("responce.txt", 'w') as f:
-    f.write(str(responce.status_code))
+    with open(filename, 'w') as f:
+        f.write(str(responce.status_code))
 
-print(prettybody)
+    print(prettybody)
+
+
+####
+
+
+sendResult("iops_responce.txt",iops_samples, start_iso[0], "{}-{}".format(test_type,jobfile))
+sendResult("lat_responce.txt",lat_samples, start_iso[0], "{}-{}-latency".format(test_type,jobfile))
